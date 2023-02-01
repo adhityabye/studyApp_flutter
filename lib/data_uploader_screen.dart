f6409a1eee919aa89ad67c6ecdf9144badcbe839
Abinnnn <mr.adhityabayu@gmail.com>
2023-02-01 23:30:10 +0700

    temp main screen

---
+++ lib/data_uploader_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+import 'controllers/question_paper/data_uploader.dart';
+
+class DataUploaderScreen extends StatelessWidget {
+  DataUploaderScreen({Key? key}) : super(key: key);
+  DataUploader controller = Get.put(DataUploader());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text(
+        "Uploading",
+      )),
+    );
+  }
+}
