fbeaf07a615ca3b884551a8ebed0c94d9d7ed977
Abinnnn <mr.adhityabayu@gmail.com>
2023-02-03 22:19:21 +0700

    dark theme color

---
+++ lib/configs/themes/app_dark_themes.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import "package:flutter/cupertino.dart";
+
+const Color primaryDarkColorDark = Color(0xFF2e3c62);
+const Color primaryColorDark = Color(0xFF99ace1);
