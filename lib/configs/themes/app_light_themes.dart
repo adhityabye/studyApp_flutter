8da103c3a5ea09fd70eaecead5a4be5ca925155a
Abinnnn <mr.adhityabayu@gmail.com>
2023-02-03 22:19:58 +0700

    light theme color

---
+++ lib/configs/themes/app_light_themes.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import "package:flutter/cupertino.dart";
+
+const Color primaryLightColorLight = Color(0xFF3ac3cb);
+const Color primaryColorLight = Color(0xFFFf85187);
