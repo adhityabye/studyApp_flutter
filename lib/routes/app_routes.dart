24ac6f1258580920a674de3811b72e838fa7bf09
Abinnnn <mr.adhityabayu@gmail.com>
2023-02-03 23:07:17 +0700

    routes screens

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,17 @@
+import 'package:study_app_flutter/screens/introduction/introduction.dart';
+
+import '../screens/splash/splash_screen.dart';
+import 'package:get/get.dart';
+
+class AppRoutes {
+  static List<GetPage> routes() => [
+        GetPage(
+          name: "/",
+          page: () => SplashScreen(),
+        ),
+        GetPage(
+          name: "/introduction",
+          page: () => AppIntroductionScreen(),
+        ),
+      ];
+}
