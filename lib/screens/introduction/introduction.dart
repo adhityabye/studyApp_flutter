29196ef05b940b9a4c0cd923c3274870963199c8
Abinnnn <mr.adhityabayu@gmail.com>
2023-02-03 22:24:07 +0700

    introduction screen ui

---
+++ lib/screens/introduction/introduction.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:study_app_flutter/widgets/app_circle_button.dart';
+import 'package:get/get.dart';
+
+class AppIntroductionScreen extends StatelessWidget {
+  const AppIntroductionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: Get.width * 0.2),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.star, size: 65, color: Colors.amber),
+              SizedBox(height: 40),
+              const Text(
+                  "this is a study app. You can use it as you want. If you understand how its works, you would able to scale it"),
+              SizedBox(height: 40),
+              AppCircleButton(
+                  onTap: () => null,
+                  child: Icon(Icons.arrow_forward, size: 35)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
