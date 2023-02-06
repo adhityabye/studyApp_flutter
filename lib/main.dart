d4bc66ae261599231e9036226900c7b405b6a470
Abinnnn <mr.adhityabayu@gmail.com>
2023-02-06 23:38:56 +0700

    theme controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:study_app_flutter/bindings/initial_bindings.dart';
+import 'package:study_app_flutter/configs/themes/app_light_themes.dart';
+import 'package:study_app_flutter/controllers/theme_controller.dart';
 import 'package:study_app_flutter/data_uploader_screen.dart';
 import 'package:get/get.dart';
 import 'package:study_app_flutter/routes/app_routes.dart';
 import 'package:study_app_flutter/screens/introduction/introduction.dart';
 import 'package:study_app_flutter/screens/splash/splash_screen.dart';
+import 'configs/themes/app_dark_themes.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -21,6 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: Get.find<ThemeController>().darkTheme,
       getPages: AppRoutes.routes(),
     );
   }
