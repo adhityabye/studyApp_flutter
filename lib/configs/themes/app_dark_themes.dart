47b44459ff6c4fc80431fbd2a5dd312700a2b100
Abinnnn <mr.adhityabayu@gmail.com>
2023-02-06 23:41:04 +0700

    dark theme color update

--- lib/configs/themes/app_dark_themes.dart
+++ lib/configs/themes/app_dark_themes.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 import "package:flutter/cupertino.dart";
+import 'package:study_app_flutter/configs/themes/sub_theme_data_mixin.dart';
+import 'package:get/get.dart';
+import 'dart:ffi';
 
-const Color primaryDarkColorDark = Color(0xFF2e3c62);
+const Color primaryDarkColorDark = Color.fromARGB(255, 33, 47, 84);
 const Color primaryColorDark = Color(0xFF99ace1);
+const Color mainTextColorDark = Colors.white;
+
+class DarkTheme with SubThemeData {
+  ThemeData buildDarkTheme() {
+    final ThemeData systemDarkTheme = ThemeData.dark();
+    return systemDarkTheme.copyWith(
+      iconTheme: getIconTheme(),
+      textTheme: getTextThemes(),
+    );
+  }
+}
