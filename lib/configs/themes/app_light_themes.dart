79a4fbd8cff6720149d450f5ca66228f58d1646f
Abinnnn <mr.adhityabayu@gmail.com>
2023-02-06 23:41:24 +0700

    light theme color update

--- lib/configs/themes/app_light_themes.dart
+++ lib/configs/themes/app_light_themes.dart
@@ -1,5 +1,20 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import "package:flutter/cupertino.dart";
+import 'package:study_app_flutter/configs/themes/sub_theme_data_mixin.dart';
+import 'package:get/get.dart';
 
 const Color primaryLightColorLight = Color(0xFF3ac3cb);
 const Color primaryColorLight = Color(0xFFFf85187);
+const Color mainTextColor = Color.fromARGB(255, 40, 40, 40);
+
+class LightTheme with SubThemeData {
+  buildLightTheme() {
+    final ThemeData systemLightTheme = ThemeData.light();
+    return systemLightTheme.copyWith(
+      iconTheme: getIconTheme(),
+      textTheme: getTextThemes(),
+    );
+  }
+}
