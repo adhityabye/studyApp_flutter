8882ccfc0774b0be3872f26e0727dfdc0eb1e078
Abinnnn <mr.adhityabayu@gmail.com>
2023-02-07 12:55:41 +0700

    bindings controller

--- lib/bindings/initial_bindings.dart
+++ lib/bindings/initial_bindings.dart
@@ -1,9 +1,14 @@
 import 'package:get/get.dart';
 import 'package:study_app_flutter/controllers/auth_controller.dart';
 
+import '../controllers/theme_controller.dart';
+
 class InitialBindings implements Bindings {
   @override
   void dependencies() {
     Get.put(AuthController(), permanent: true);
+    Get.put(
+      ThemeController(),
+    );
   }
 }
